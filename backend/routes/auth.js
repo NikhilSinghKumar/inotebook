bebeb56b8793558c2166952cef74adb3bfd1e178
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-16 01:32:29 +0530

    hashing password using bcryptjs & JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = '$$richmentality$$richlife$$royal$$';
 
 // Create a User using: POST "/api/auth/createuser" . No login required
 // use POST otherwise login data might be seen in computer log, also for large Data
@@ -21,13 +25,24 @@ router.post('/createuser', [
         if(user){
             return res.status(400).json({error: "Sorry this email exists already."})
         }
+        // Password Gen salt
+        const salt = await bcrypt.genSalt(10);
+        // Password hashing
+        const secPass = await bcrypt.hash(req.body.password, salt);
         // create a new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
         })
-        res.json({user})
+        const data = {
+            user: {
+                id : user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken});
+        // res.json({user})
     } 
     // catch error
     catch (error){

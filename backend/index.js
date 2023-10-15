a62cbd9188884731b91ea52cf3c5d26cf4a681d2
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-15 17:40:32 +0530

    routes, schema & model

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// available routes
+app.use('/api/auth', require('./routes/auth'))
+// app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

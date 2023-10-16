b9b13071b4dc5ace87f2a34e079d2af0c3954677
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-17 00:55:37 +0530

    creating a middleware to decode user from a JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require("../middleware/fetchuser");
 
 const JWT_SECRET = '$$richmentality$$richlife$$royal$$';
 
-// Create a User using: POST "/api/auth/createuser" . No login required
+// ROUTE 1: Create a User using: POST "/api/auth/createuser" . No login required
 // use POST otherwise login data might be seen in computer log, also for large Data
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
@@ -51,7 +52,7 @@ router.post('/createuser', [
     }
 })
 
-// Authenticate a User using: POST "/api/auth/login" . No login required
+// ROUTE 2: Authenticate a User using: POST "/api/auth/login" . No login required
 router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists()
@@ -85,6 +86,22 @@ router.post('/login', [
         console.error(error.message);
         res.status(500).send("Internal server error occurred.")
     }
+})
+
+// ROUTE 3: Get loggedin User details using: POST "/api/auth/getuser" . Login required
+// JWT token need to send here
+router.post('/getuser', fetchuser, async (req,res) =>{
+
+try{
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user)
+} 
+catch (error){
+    console.error(error.message);
+    res.status(500).send("Internal server error occurred.")
+}
+
 })
 
 module.exports = router

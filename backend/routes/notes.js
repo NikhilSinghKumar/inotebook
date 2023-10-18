6f8dd5fefb5b6684551f9092949a65ae7ec1c256
Nikhil Kumar <nikhilsk369@gmail.com>
2023-10-19 00:31:09 +0530

    Updating an existing Note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -38,4 +38,27 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+// ROUTE 3: Update an existing Note using: PUT "/api/notes/updatenote" . login required
+router.put('/updatenote/:id', fetchuser, async (req,res) =>{
+    // Destructuring
+    const {title, description, tag} = req.body;
+
+    // create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")};
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed")
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+    res.json({note})
+})
+
+
 module.exports = router
